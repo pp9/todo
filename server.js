173ed7469c7e022c966bf5581d2712923b0bb333
esimonenko <eugeny.simonenko@hiqo-solutions.com>
2015-05-26 18:03:04 +0300

    add

---
+++ server.js
@@ -0,0 +1,23 @@
+var http = require('http');
+var fs = require('fs');
+var mime = require('mime');
+var clientPatch = 'client/';
+
+http.createServer(function(req, res) {
+	var reqPatch = req.url;
+	if(reqPatch === '/') {
+		fs.readFile(clientPatch + 'index.html', function(err, data) {
+			if(err) return err;
+			res.end(data)
+		});
+	}else {
+		var fileType = mime.lookup(clientPatch + reqPatch)
+		fs.readFile(clientPatch + reqPatch, function(err, data) {
+			if(err) return err;
+			res.writeHead(200, {
+				'Content-Type':  fileType
+			});
+			res.end(data)
+		});
+	}
+}).listen(8000);
