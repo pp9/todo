cd9a1af85c8d1b80fe23fde43985c1a1197388f2
pp9 <eugeny.simonenko@hiqo-solutions.com>
2015-05-31 18:10:08 +0300

    add

--- server.js
+++ server.js
@@ -4,23 +4,43 @@ var mime = require('mime');
 var clientPatch = 'client/';
 
 http.createServer(function(req, res) {
+	var reqMethod = req.method;
 	var reqPatch = req.url;
-	if(reqPatch === '/') {
-		fs.readFile(clientPatch + 'index.html', function(err, data) {
-			if(err) return err;
-			res.end(data)
-		});
-	}else {
-		var fileType = mime.lookup(clientPatch + reqPatch);
-		console.log(clientPatch + reqPatch);
+	
+	switch(reqMethod) {
+		// GET
+		case 'GET':
+			if(reqPatch === '/') {
+				fs.readFile(clientPatch + 'index.html', function(err, data) {
+					if(err) return err;
+					res.end(data)
+				});
+			}else {
+				var fileType = mime.lookup(clientPatch + reqPatch);
+				console.log(clientPatch + reqPatch);
 
-		fs.readFile(clientPatch + reqPatch, function(err, data) {
-			if(err) return err;
-			res.writeHead(200, {
-				'Content-Type': fileType
-			});
-			// console.log(data);
-			res.end(data)
-		});
+				fs.readFile(clientPatch + reqPatch, function(err, data) {
+					if(err) return err;
+					res.writeHead(200, {
+						'Content-Type': fileType
+					});
+					// console.log(data);
+					res.end(data)
+				});
+			}
+		break;
+	// GET
+	case 'POST':
+		if(reqPatch == '/save_db') {
+			req.on('data', function(data){
+				fs.writeFile(clientPatch + 'new.json', data);
+				// console.log('' + data);
+			})
+			// console.log(req);
+			res.end('GOOD')
+		}
+	break;
 	}
+	
+
 }).listen(8000);
