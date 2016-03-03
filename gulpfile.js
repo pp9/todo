0f7f01dc70a837a7f6e0837d6284ca724ab45519
esimonenko <esimonenko@fortyfour.com>
2016-03-03 16:19:54 +0300

    merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+function swallowError (error) {
+
+    //If you want details of the error in the console
+    console.log(error.toString());
+
+    this.emit('end');
+}
+
 gulp.task('sass', function () {
   return gulp.src('./client/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .on('error', swallowError)
     .pipe(gulp.dest('./client/css'));
 });
 gulp.task('watch', ['sass'], function () {
